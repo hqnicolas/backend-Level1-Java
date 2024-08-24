b4996f14d95f9d57b97e929955c654b8c280574f
Nicolas Pereira <hqnicolas@msn.com>
2024-08-24 18:01:05 -0300

    exercicio03

---
+++ portifolio-backend/exercicio03/src/main/java/org/example/Personagem.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Personagem {
+    private String nome;
+    private int vida;
+    private int ataque;
+
+    public Personagem(String nome, int vida, int ataque) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+    }
+
+    public void receberDano(int dano) {
+        vida -= dano;
+        if (vida <= 0) {
+            vida = 0;
+            System.out.println(nome + " foi derrotado!");
+        }
+    }
+
+    public void atacar(Personagem alvo) {
+        if (alvo.vida > 0) {
+            alvo.receberDano(this.ataque);
+        } else {
+            System.out.println(nome + " não pode atacar " + alvo.nome + " porque está derrotado.");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+}
